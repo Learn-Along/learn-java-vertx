8404268e3aa09e38bf3331e43ea1a81d69068828
Martin Ahindura <martinahindura@gmail.com>
2020-01-26 00:49:12 +0300

    Add the vertx-wiki folder

---
+++ vertx-wiki/src/main/java/com/sopherapps/learn/MainVerticle.java
@@ -0,0 +1,11 @@
+package com.sopherapps.learn;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+
+public class MainVerticle extends AbstractVerticle {
+    @Override
+    public void start(Promise<Void> promise){
+        promise.complete();
+    }
+}
